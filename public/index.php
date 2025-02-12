555eb62febe596a20e6466d6e4af40223bef54ed
Ilshat Khabirov <khabdev@yandex.ru>
2025-02-12 04:54:14 +0300

    add layout and index page

--- public/index.php
+++ public/index.php
@@ -2,13 +2,29 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
-$app = AppFactory::create();
+$container = new Container();
+
+$container->set('renderer', function () {
+    $renderer = new PhpRenderer(__DIR__ . '/../templates', ['title' => 'My App']);
+    $renderer->setLayout('layout.phtml');
+    return $renderer;
+});
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-});
+    $viewData = [
+        'name' => 'John',
+    ];
+
+    return $this->get('renderer')->render($response, 'index.phtml', $viewData);
+})->setName('hello');
+
 $app->run();
